4e1ab2e6beaa368b6d4ade468e45f614c00db4fe
AllanSechrist <sechristallan@gmail.com>
2022-09-14 23:34:23 +0900

    add tests for Book model

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Book
+
+
+class BookTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.book = Book.objects.create(
+            title="test title",
+            series="",
+            publisher="test publisher",
+            author="test author",
+            isbn="1234567890123",
+        )
+
+    def test_book_content(self):
+        self.assertEqual(self.book.title, "test title")
+        self.assertEqual(self.book.series, "")
+        self.assertEqual(self.book.publisher, "test publisher")
+        self.assertEqual(self.book.author, "test author")
+        self.assertEqual(self.book.isbn, "1234567890123")
