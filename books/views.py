c5eb7b6fe16778f4edd80fe8aaea034de5506e74
AllanSechrist <sechristallan@gmail.com>
2022-09-26 21:46:33 +0900

    change endpoints to display books and rooms related to the currently logged in user

--- books/views.py
+++ books/views.py
@@ -1,14 +1,16 @@
 from rest_framework import viewsets
 
-from .models import Book
 from .permissions import IsOwnerOrReadOnly
 from .serializers import BookSerializer
 
 
 class BookViewSet(viewsets.ModelViewSet):
     permission_classes = (IsOwnerOrReadOnly,)
-    queryset = Book.objects.all()  # Change to user only books in future
+    # queryset = Book.objects.all()  # Change to user only books in future
     serializer_class = BookSerializer
+
+    def get_queryset(self):
+        return self.request.user.books.all()
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
--- rooms/views.py
+++ rooms/views.py
@@ -1,14 +1,16 @@
 from rest_framework import viewsets
 
-from .models import Room
 from .permissions import IsOwnerOrReadOnly
 from .serializers import RoomSerializer
 
 
 class RoomViewSet(viewsets.ModelViewSet):
     permission_classes = (IsOwnerOrReadOnly,)
-    queryset = Room.objects.all()  # user only in future
+    # queryset = Room.objects.all()  # user only in future
     serializer_class = RoomSerializer
+
+    def get_queryset(self):
+        return self.request.user.rooms.all()
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
